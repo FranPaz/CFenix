0aba970d395b437d23f196f706068962c0125fc2
iafar86 <iafar86@hotmail.com>
2014-10-06 08:58:43 -0300

    Mods en trabajos e insumos

--- CFenix_Dev/Scripts/App/Trabajos/trabajosCtrl.js
+++ CFenix_Dev/Scripts/App/Trabajos/trabajosCtrl.js
@@ -1,1 +1,27 @@
-﻿
+﻿copisteriaFenixApp.controller('trabajosCtrl', function ($scope, $modal, $stateParams, $state) {
+
+
+
+
+
+
+    //#region llamadas a partials de tabs en Dashboard de Trabajos
+    $scope.trabajosAdd = function () {
+        $state.go('Trabajos.add');
+    };
+
+    $scope.trabajosMod = function () {
+        $state.go('Trabajos.mod');
+    };
+
+    //$scope.trabajosDetail = function () {
+    //    $state.go('Trabajos.detail');
+    //};
+    //#endregion
+
+
+});
+
+
+
+
