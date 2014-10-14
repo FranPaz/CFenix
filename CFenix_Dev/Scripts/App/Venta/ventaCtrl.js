6e5dfc9f5836ed35ff7a3dd079eb6a3cac9a4c87
FranPaz <francisco_paz3@hotmail.com>
2014-10-14 11:31:18 -0300

    Agregado javascript ventaCtrl

---
+++ CFenix_Dev/Scripts/App/Venta/ventaCtrl.js
@@ -0,0 +1,4 @@
+﻿copisteriaFenixApp.controller('ventaCtrl', function ($scope, clientesDataFactory, $stateParams, $state) {
+    $scope.detallesFactura; //fpaz: recive array de detalles de una factura en particular o se usar para ir cargando con los productos que se van agregando a la venta
+    
+});
